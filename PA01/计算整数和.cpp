0bdb5763a6d5f1dd54163f4b6ece5968ef960172
DonaldTrump1a <88626701+DonaldTrump1a@users.noreply.github.com>
2022-03-02 12:34:28 +0800

    Update 计算整数和.cpp

--- PA01/计算整数和.cpp
+++ PA01/计算整数和.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
 
+int main(void) {
+	int max, min;
+	int j = 0;
+	cin >> min >> max;
+
+	for (int i = min; i <= max; i++) {
+		j += i;
+	}
+	cout << j;
+	return 0;
+}
