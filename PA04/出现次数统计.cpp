3703c5067163b37abcabd1eb40aa3657a394b465
DonaldTrump1a <88626701+DonaldTrump1a@users.noreply.github.com>
2022-03-21 16:23:44 +0800

    Update 出现次数统计.cpp

--- PA04/出现次数统计.cpp
+++ PA04/出现次数统计.cpp
@@ -5,7 +5,7 @@
 
 #include <iostream>
 using namespace std;
-const int MAX = 10000;
+const int MAX = 1000;
 int counter[MAX];
 
 int main(void)
@@ -18,7 +18,7 @@ int main(void)
 		cin >> a;
 		counter[a]++;
 	}
-	for (int i = 0; i <= 10000; i++) {
+	for (int i = 0; i <= 1000; i++) {
 		if (counter[i] > flag) {
 			flag = counter[i];
 			res = i;
