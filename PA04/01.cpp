4feeafd0c139be816a8afe01837b8f851c04caae
DonaldTrump1a <88626701+DonaldTrump1a@users.noreply.github.com>
2022-03-21 16:16:50 +0800

    Add files via upload

---
+++ PA04/01.cpp
@@ -0,0 +1,29 @@
+﻿//
+//  Created by 张益铭 on 03/21/22.
+//  Copyright © 2022 张益铭. All Rights Reserved.
+//
+
+#include <iostream>
+using namespace std;
+const int MAX = 10000;
+int counter[MAX];
+
+int main(void)
+{
+	int N, res;
+	int flag = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		counter[a]++;
+	}
+	for (int i = 0; i <= 10000; i++) {
+		if (counter[i] > flag) {
+			flag = counter[i];
+			res = i;
+		}
+	}
+	cout << res << endl;
+	return 0;
+}
