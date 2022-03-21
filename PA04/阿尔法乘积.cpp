72f0ec518d64805c23d560df63100bbe2fb4cbb5
DonaldTrump1a <88626701+DonaldTrump1a@users.noreply.github.com>
2022-03-21 16:36:47 +0800

    Create 阿尔法乘积.cpp

---
+++ PA04/阿尔法乘积.cpp
@@ -0,0 +1,33 @@
+//
+//  Created by 张益铭 on 03/21/22.
+//  Copyright © 2022 张益铭. All Rights Reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	long unsigned int num;
+	cin >> num;
+
+	int flag;
+	int prod = 1;
+	while (num != 0) {
+		if (num % 10 == 0) {
+			num /= 10;
+		}
+		else {
+			flag = num % 10;
+			num /= 10;
+			prod *= flag;
+		}
+		if (num == 0 && prod / 10 != 0) {
+			num = prod;
+			prod = 1;
+		}
+	}
+
+	cout << prod << endl;
+	return 0;
+}
