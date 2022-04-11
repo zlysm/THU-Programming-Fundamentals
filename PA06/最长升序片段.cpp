0217d5b5380648cf8e9cf478bb4351937ea0eee3
DonaldTrump1a <88626701+DonaldTrump1a@users.noreply.github.com>
2022-04-11 23:20:38 +0800

    Update 最长升序片段.cpp

--- PA06/最长升序片段.cpp
+++ PA06/最长升序片段.cpp
@@ -3,21 +3,16 @@
 //Copyright (C) 张益铭 2022. All Rights Reserved.
 //
 
-#include <iostream>
-#include<cstring>
-
-using namespace std;
+#include <cstdio>
 
 int main() {
     char str[99];
-    cin.getline(str, 99);
-    int length = strlen(str);
-    
+    scanf("%s", str);
     int max = 0, count = 1;
-    for (int i = 0; i < length - 1; i++) {
+    for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] <= str[i + 1]) {
             count++;
-        } else {  //重置
+        } else {
             if (count > max) {
                 max = count;
             }
@@ -27,7 +22,7 @@ int main() {
     if (count > max) {
         max = count;
     }
-    cout << max << endl;
+    printf("%d\n", max);
 
     return 0;
 }
